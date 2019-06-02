af933acfc86c3fe9f99548ea82fd3d581941a577
ragewolf254 <ragewolf254@yahoo.com>
2019-06-03 01:25:34 +0800

    Drafted for achievements

--- src/components/core/Achievements/Achievements.vue
+++ src/components/core/Achievements/Achievements.vue
@@ -8,127 +8,63 @@
       <v-layout
         row
         wrap
+        justify-center 
+        ma-0
       >
         <v-flex
-          xs6
-          class="text-md-center text-sm-center text-xs-center"
+          md4
+          class="text-md-center text-sm-center text-xs-center ml-5 pa-0"
         >
-          <div
-            class="display-1"
-            style="background-color: pink"
-            dark
+          <v-img
+            :src="`${bgPath}bg-years-in-business.png`"
+            style="width:50%;
+            height: auto;
+            contain
+            "
           >
+            <div
+              class="display-1"
+              style="
+             position: relative;
+             top:50%;
+             transform: translateY(-50%);
+            "
+            >
+              11
+              <p class="body-1">
+                YEARS IN BUSINESS
+              </p>
+            </div>
+          </v-img>
+        </v-flex>
+
+        <v-flex
+          md4
+          class="text-md-center text-sm-center text-xs-center justify-center pa-0 mt-5"
+          style="
+            position: relative;
+            transform: translateX(-70%);
+          "
+        >
+          <div class="display-1">
             23K
-            <div class="body-1">SQM TURNED OVER</div>
+            <p class="body-1">
+              SQM TURNED OVER
+            </p>
           </div>
         </v-flex>
-
-        <v-flex
-          xs6
-          class="text-md-center text-sm-center text-xs-center"
-        >
-          <div
-            class="display-1"
-            style="background-color: red"
-            dark
-          >
-            11
-            <div class="body-1">YEARS IN BUSINESS</div>
-          </div>
-        </v-flex>
-
-        <v-flex
-          xs6
-          class="text-md-center text-sm-center text-xs-center"
-        >
-          <div
-            class="display-1"
-            style="background-color: blue"
-            dark
-          >
-            2.2
-            <div class="body-1">TERRABYTES OF DATA</div>
-          </div>
-        </v-flex>
-
-        <v-flex
-          xs6
-          class="text-md-center text-sm-center text-xs-center"
-        >
-          <div
-            class="display-1"
-            style="background-color: yellow"
-            dark
-          >
-            17
-            <div class="body-1">UNIQUE CITIES</div>
-          </div>
-        </v-flex>
-
-        <v-flex
-          xs6
-          class="text-md-center text-sm-center text-xs-center"
-        >
-          <div
-            class="display-1"
-            style="background-color: cyan"
-            dark
-          >
-            8
-            <div class="body-1">OFFICE SPACES</div>
-          </div>
-        </v-flex>
-
-        <v-flex
-          xs6
-          class="text-md-center text-sm-center text-xs-center"
-        >
-          <div
-            class="display-1"
-            style="background-color: teal"
-            dark
-          >
-            54
-            <div class="body-1">RESIDENTIAL HOUSES</div>
-          </div>
-        </v-flex>
-
-        <v-flex
-          xs6
-          class="text-md-center text-sm-center text-xs-center"
-        >
-          <div
-            class="display-1"
-            style="background-color: violet"
-            dark
-          >
-            84
-            <div class="body-1">CONDOMINIUM UNITS</div>
-          </div>
-        </v-flex>
-
-        <v-flex
-          xs6
-          class="text-md-center text-sm-center text-xs-center"
-        >
-          <div
-            class="display-1"
-            style="background-color: green"
-            dark
-          >
-            34
-            <div class="body-1">COMMERCIAL SPACES</div>
-          </div>
-        </v-flex>
-
       </v-layout>
     </v-container>
   </div>
 </template>
 
 <script>
+import { bgPath } from '@/utils/path'
 
 export default {
-  name: 'achievements'
+  name: 'achievements',
+  computed: {
+    bgPath: () => bgPath
+  }
 }
 </script>
