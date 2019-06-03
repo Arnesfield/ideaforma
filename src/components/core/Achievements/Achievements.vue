c9d257c0e64b3cb1ad108b314092c683c97aef90
ragewolf254 <ragewolf254@yahoo.com>
2019-06-03 23:01:22 +0800

    Finished layout for achievements. Decided to make it 3 rows

--- src/components/core/Achievements/Achievements.vue
+++ src/components/core/Achievements/Achievements.vue
@@ -1,68 +1,159 @@
 <template>
-  <div>
-    <v-layout/>
-    <v-container
-      fluid
-      grid-list-lg
+  <v-container
+    fluid
+    grid-list-lg
+  >
+    <v-layout
+      row
+      align-center
+      justify-center 
+      ma-0
     >
-      <v-layout
-        row
-        wrap
-        justify-center 
-        ma-0
+      <v-flex
+        md4
+        class="text-xs-center ml-5 pa-0"
       >
-        <v-flex
-          md4
-          class="text-md-center text-sm-center text-xs-center ml-5 pa-0"
-        >
-          <v-img
-            :src="`${bgPath}bg-years-in-business.png`"
-            style="width:50%;
-            height: auto;
-            contain
-            "
-          >
-            <div
-              class="display-1"
-              style="
-             position: relative;
-             top:50%;
-             transform: translateY(-50%);
-            "
-            >
-              11
-              <p class="body-1">
-                YEARS IN BUSINESS
-              </p>
-            </div>
-          </v-img>
-        </v-flex>
+        <!-- TODO: separate this to component -->
+        <img-hexa
+          :src="`${bgPath}bg-years-in-business.png`"
+          title="11"
+          text="Years in Business"
+        />
+      </v-flex>
 
-        <v-flex
-          md4
-          class="text-md-center text-sm-center text-xs-center justify-center pa-0 mt-5"
-          style="
-            position: relative;
-            transform: translateX(-70%);
-          "
-        >
-          <div class="display-1">
-            23K
-            <p class="body-1">
-              SQM TURNED OVER
-            </p>
-          </div>
-        </v-flex>
-      </v-layout>
-    </v-container>
-  </div>
+      <v-flex
+        md4
+        class="text-xs-center"
+      >
+        <v-container>
+          <v-layout
+            column
+            align-center
+            justify-center
+          >
+            <h1 class="display-3">23K</h1>
+            <p class="body-1 text-capitalize"> sqm turned over</p>
+          </v-layout>
+        </v-container>
+      </v-flex>
+
+      <v-flex
+        md3
+        class="text-xs-center"
+      >
+        <v-container>
+          <v-layout
+            column
+            align-center
+            justify-center
+          >
+            <h1 class="display-3">2.2</h1>
+            <p class="body-1 text-capitalize">Terabytes of Data</p>
+          </v-layout>
+        </v-container>
+      </v-flex>
+    </v-layout>
+
+    <!-- Second Row -->
+
+    <v-layout
+      row
+      align-center
+      justify-center 
+      ma-0
+    >
+      <v-flex
+        md4
+        class="text-xs-center ml-5 pa-0"
+      >
+        <img-hexa
+          :src="`${bgPath}bg-residential-houses.png`"
+          title="54"
+          text="Residential Houses"
+        />
+      </v-flex>
+
+      <v-flex md4>
+        <v-container>
+          <v-layout 
+            column
+            align-center
+            justify-center
+          >
+            <h1
+              v-text="84"
+              class="display-3"
+            />
+            <p
+              v-text="`Condominium Units`"
+              class="body-1"
+            />
+          </v-layout>
+        </v-container>
+      </v-flex>
+
+      <v-flex
+        md4
+        class="text-xs-center ml-5 pa-0"
+        align-center
+        justify-center
+      >
+        <!-- TODO: separate this to component -->
+        <img-hexa
+          :src="`${bgPath}bg-unique-cities.png`"
+          title="17"
+          text="Unique Cities"
+        />
+      </v-flex>
+    </v-layout>
+    <!-- Third Row -->
+    <v-layout
+      row
+      align-center
+      justify-center
+      ma-0
+    >
+      <v-flex
+        md6
+        class="text-xs-center"
+      >
+        <v-container>
+          <v-layout
+            column
+            align-center
+            justify-center
+          >
+            <h1 class="display-3">23K</h1>
+            <p class="body-1 text-capitalize"> sqm turned over</p>
+          </v-layout>
+        </v-container>
+      </v-flex>
+
+      <v-flex
+        md6
+        class="text-xs-center ml-5 pa-0"
+      >
+        <!-- TODO: separate this to component -->
+        <img-hexa
+          :src="`${bgPath}bg-residential-houses.png`"
+          title="8"
+          text="Office Spaces"
+        />
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
 import { bgPath } from '@/utils/path'
+import ImgHexa from './components/ImgHexa'
 
 export default {
   name: 'achievements',
+  components: {
+    ImgHexa
+  },
+
   computed: {
     bgPath: () => bgPath
   }
---
+++ src/components/core/Achievements/components/ImgHexa.vue
@@ -0,0 +1,44 @@
+<template>
+  <v-img
+    v-bind="{ src }"
+    style="width: 50%;"
+  >
+    <v-container fill-height>
+      <v-layout
+        column
+        align-center
+        justify-center
+      >
+        <h1
+          v-text="title"
+          class="display-3"
+        />
+        <p
+          v-text="text"
+          class="body-1 text-capitalize"
+        />
+      
+      </v-layout>
+    </v-container>
+  </v-img>
+</template>
+
+<script>
+export default {
+  name: 'img-hexa',
+  props: {
+    src: {
+      type: String,
+      required: true
+    },
+    title: {
+      type: String,
+      required: true
+    },
+    text: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
